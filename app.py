974e98099dc55dd46f663a8056b9db38fcc02eb1
Sarang Gami <121340232+SarangGami@users.noreply.github.com>
2023-12-09 09:27:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
-pip install streamlit_pandas_profiling
 from pandas_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 
